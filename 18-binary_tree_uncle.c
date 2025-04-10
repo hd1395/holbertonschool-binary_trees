5e5f4288f57582e5f57e84ed62cbe4f151a44263
Hamad <Hdahash@hotmail.com>
2025-04-10 12:06:50 +0300

    add 18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+
+/**
+ * binary_tree_uncle- finds the uncle of a node
+ * @node: pointer to the node to find its uncle
+ * Return: the sibling of the node, NULL otherwise
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+return (NULL);
+if (node->parent->parent == NULL)
+return (NULL);
+if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
+return (NULL);
+if (node->parent->parent->left == node->parent)
+return (node->parent->parent->right);
+return (node->parent->parent->left);
+}
+
