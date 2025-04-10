0ab6d80ab985bfa0e5fd5bf91f0537c26b06bd59
Hamad <Hdahash@hotmail.com>
2025-04-10 12:59:05 +0300

    issue checking perfect tree - 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -46,7 +46,8 @@ return (1);
 
 if ((tree->left) && (tree->right))
 return ((binary_tree_height(tree->left) == binary_tree_height(tree->right))
-&& ((tree->left) && binary_tree_is_perfect(tree->right)));
+&& (binary_tree_is_perfect(tree->left)
+&& binary_tree_is_perfect(tree->right)));
 return (0);
 
 }
