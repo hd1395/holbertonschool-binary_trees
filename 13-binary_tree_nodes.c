87c518c9d847a91cdd5b8289470b39edea0a85fb
Hamad <Hdahash@hotmail.com>
2025-04-10 10:19:05 +0300

    fix code style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,11 +8,11 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-if(!tree)
+if (!tree)
 return (0);
 
-return ((tree->left || tree->right) ? 1 : 0) +
+return (((tree->left || tree->right) ? 1 : 0) +
 binary_tree_nodes(tree->left) +
-binary_tree_nodes(tree->right);
+binary_tree_nodes(tree->right));
 
 }
